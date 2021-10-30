0bccdcee368f72be08233db87f05d2c1b9fbfdbb
auddl0756 <cjddl789123@naver.com>
2021-10-30 19:54:22 +0900

    패스워드 인코더 설정

--- src/main/java/com/roon/apiservice/config/SecurityConfig.java
+++ src/main/java/com/roon/apiservice/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.roon.apiservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
