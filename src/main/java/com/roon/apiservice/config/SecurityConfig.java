0631adb09040889508d8d579bd59689225f4c472
auddl0756 <cjddl789123@naver.com>
2021-10-30 19:55:16 +0900

    리소스(url) 접근 권한 설정

--- src/main/java/com/roon/apiservice/config/SecurityConfig.java
+++ src/main/java/com/roon/apiservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.roon.apiservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,5 +12,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/sample/add").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+
+        http.formLogin();
     }
 }
