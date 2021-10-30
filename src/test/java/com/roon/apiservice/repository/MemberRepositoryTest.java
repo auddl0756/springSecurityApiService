4b101cecf32155bb22128d402dc50f145b1818ca
auddl0756 <cjddl789123@naver.com>
2021-10-30 20:26:22 +0900

    dao 테스트/더미 데이터 추가

---
+++ src/test/java/com/roon/apiservice/repository/MemberRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.roon.apiservice.repository;
+
+import com.roon.apiservice.entity.Member;
+import com.roon.apiservice.entity.MemberRole;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    //테스트라 보긴 어렵고, 더미데이터 추가하는 용도.
+    @Test
+    public void insertDummies() {
+        IntStream.rangeClosed(1, 100).forEach(idx -> {
+            Member member = Member.builder()
+                    .email("user" + idx + "@naver.com")
+                    .name("user" + idx)
+                    .password(passwordEncoder.encode("1111"))
+                    .build();
+
+            if (idx < 50) {
+                member.setMemberRole(MemberRole.GUEST);
+            } else if (idx < 95) {
+                member.setMemberRole(MemberRole.MEMBER);
+            } else {
+                member.setMemberRole(MemberRole.ADMIN);
+            }
+
+            memberRepository.save(member);
+        });
+    }
+
+
+}
