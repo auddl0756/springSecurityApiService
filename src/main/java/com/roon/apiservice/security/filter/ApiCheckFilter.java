e09536fb5fd2183d4652d6e93530109f52f3dad5
auddl0756 <cjddl789123@naver.com>
2021-11-01 18:37:02 +0900

    보안 토큰 검사하는 필터

--- src/main/java/com/roon/apiservice/security/filter/ApiCheckFilter.java
+++ src/main/java/com/roon/apiservice/security/filter/ApiCheckFilter.java
@@ -1,7 +1,9 @@
 package com.roon.apiservice.security.filter;
 
 import lombok.extern.log4j.Log4j2;
+import org.json.simple.JSONObject;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,13 +11,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Log4j2
 public class ApiCheckFilter extends OncePerRequestFilter {
     private AntPathMatcher antPathMatcher;
     private String pattern;
 
-    public ApiCheckFilter(String pattern){
+    public ApiCheckFilter(String pattern) {
         this.antPathMatcher = new AntPathMatcher();
         this.pattern = pattern;
     }
@@ -25,12 +28,43 @@ public class ApiCheckFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        if(antPathMatcher.match(pattern,request.getRequestURI())){
+        if (antPathMatcher.match(pattern, request.getRequestURI())) {
             log.info("=========ApiCheckFilter==========");
+
+            boolean checkHeader = checkAuthHeader(request);
+            if (checkHeader) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.setContentType("application/json;charset=utf-8");
+
+                JSONObject json = new JSONObject();
+                String message = "API AUTH 틀림";
+                json.put("code", "403");
+                json.put("message", message);
+
+                PrintWriter printWriter = response.getWriter();
+                printWriter.print(json);
+            }
             return;
         }
 
         filterChain.doFilter(request, response);
+    }
 
+    private static final String AUTH_NUMBER = "12345678";
+
+    private boolean checkAuthHeader(HttpServletRequest request) {
+        boolean checkResult = false;
+        String authHeader = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(authHeader)) {
+            log.info("Authorization 존재 : " + authHeader);
+            if (authHeader.equals(AUTH_NUMBER)) {
+                checkResult = true;
+            }
+        }
+
+        return checkResult;
     }
 }
