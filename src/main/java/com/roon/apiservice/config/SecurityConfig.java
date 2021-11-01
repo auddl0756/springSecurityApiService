12ad88baedcc1ea8a4faf0f3cc2325e06a12d0f2
auddl0756 <cjddl789123@naver.com>
2021-11-01 15:07:50 +0900

    filter 적용

--- src/main/java/com/roon/apiservice/config/SecurityConfig.java
+++ src/main/java/com/roon/apiservice/config/SecurityConfig.java
@@ -1,18 +1,26 @@
 package com.roon.apiservice.config;
 
 import com.roon.apiservice.entity.MemberRole;
+import com.roon.apiservice.security.filter.ApiCheckFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    //필터 bean으로 등록
+    @Bean
+    ApiCheckFilter apiCheckFilter() {
+        return new ApiCheckFilter("/posts/**/*");
     }
 
     @Override
@@ -24,5 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         http.csrf().disable();
         http.logout();
+
+        //필터 순서 조정
+        http.addFilterBefore(apiCheckFilter(), UsernamePasswordAuthenticationFilter.class); //.class는 어떤 의미지
+
     }
 }
