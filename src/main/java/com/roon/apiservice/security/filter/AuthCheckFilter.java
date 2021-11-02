2793d72fd233556fe86236d211f49989ceaf99bd
auddl0756 <cjddl789123@naver.com>
2021-11-02 15:30:36 +0900

    auth check 필터에 auth header값에 따른 처리 추가

--- src/main/java/com/roon/apiservice/security/filter/AuthCheckFilter.java
+++ src/main/java/com/roon/apiservice/security/filter/AuthCheckFilter.java
@@ -1,7 +1,9 @@
 package com.roon.apiservice.security.filter;
 
 import lombok.extern.log4j.Log4j2;
+import org.json.simple.JSONObject;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,13 +11,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+//리소스를 요청할 때 거치는 필터.
+//로그인 한 뒤의 상황이 이 필터를 올바르게 통과하는 시나리오.
 
 @Log4j2
 public class AuthCheckFilter extends OncePerRequestFilter {
     private static final AntPathMatcher antPathMatcher = new AntPathMatcher();
     private String pattern;
 
-    public AuthCheckFilter(String pattern){
+    private static final String AUTH_HEADER_VALUE = "12345678";
+
+    public AuthCheckFilter(String pattern) {
         this.pattern = pattern;
     }
 
@@ -24,11 +32,45 @@ public class AuthCheckFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        if(antPathMatcher.match(pattern,request.getRequestURI())){
+        if (antPathMatcher.match(pattern, request.getRequestURI())) {
             log.info("============== auth check filter ===============");
+
+            boolean isValidHeader = checkHttpAuthHeader(request);
+            if (isValidHeader) {
+                filterChain.doFilter(request, response);
+                return;
+            } else {
+                sendErrorToken(response);
+            }
+
             return;
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private boolean checkHttpAuthHeader(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (StringUtils.hasText(authHeader)) {
+            log.info("authorization exist : " + authHeader);
+            if (authHeader.equals(AUTH_HEADER_VALUE)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void sendErrorToken(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json;charset=utf-8");
+
+        JSONObject json = new JSONObject();
+        String message = "Authorization header value가 틀려요.";
+        json.put("code", "403");
+        json.put("message", message);
+
+        PrintWriter printWriter = response.getWriter();
+        printWriter.print(json);
     }
 }
