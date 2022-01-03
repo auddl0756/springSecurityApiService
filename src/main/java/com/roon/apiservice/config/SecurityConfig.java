324f39221ec08573e58fdb50d4559781657d84f1
auddl0756 <cjddl789123@naver.com>
2022-01-03 22:46:48 +0900

    OncePerRequestFilter 필터 체인에서 순서 조정

--- src/main/java/com/roon/apiservice/config/SecurityConfig.java
+++ src/main/java/com/roon/apiservice/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.roon.apiservice.config;
 
 import com.roon.apiservice.entity.MemberRole;
+import com.roon.apiservice.security.filter.ApiCheckFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,6 +17,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Autowired
+    private ApiCheckFilter apiCheckFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,5 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         http.csrf().disable();
         http.logout();
+
+        http.addFilterBefore(apiCheckFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
